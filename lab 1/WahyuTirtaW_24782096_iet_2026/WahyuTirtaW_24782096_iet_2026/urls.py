d236ecc267dc224baafc7e4ff5e5ba3b1c4b28e1
Wahyu Tirta Wardana <wwahyutirta@gmail.com>
2026-03-13 15:26:14 +0700

    Penambahan default urlconf/ default django

--- lab 1/WahyuTirtaW_24782096_iet_2026/WahyuTirtaW_24782096_iet_2026/urls.py
+++ lab 1/WahyuTirtaW_24782096_iet_2026/WahyuTirtaW_24782096_iet_2026/urls.py
@@ -16,13 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.views.debug import default_urlconf
 from django.http import HttpResponse
 
 def welcome(request):
     return HttpResponse("Welcome to Django!")
 
+def Blog(request):
+    return HttpResponse("This is the Blog page.")
 urlpatterns = [
+    path('', default_urlconf),
     path('admin/', admin.site.urls),
-    path('welcome/', welcome)
+    path('welcome/', welcome),
+    path('blog/', Blog),
 ]
