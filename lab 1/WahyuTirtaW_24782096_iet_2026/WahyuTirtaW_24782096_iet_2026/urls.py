ef271682856ffcd3d5fcfb962aec5c68bde6e3ca
Wahyu Tirta Wardana <wwahyutirta@gmail.com>
2026-03-14 09:32:26 +0700

    menghubungkan urls project dan app

--- lab 1/WahyuTirtaW_24782096_iet_2026/WahyuTirtaW_24782096_iet_2026/urls.py
+++ lab 1/WahyuTirtaW_24782096_iet_2026/WahyuTirtaW_24782096_iet_2026/urls.py
@@ -1,17 +1,12 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.debug import default_urlconf
 from django.http import HttpResponse
 
-def welcome(request):
-    return HttpResponse("Welcome to Django!")
 
-def Blog(request):
-    return HttpResponse("This is the Blog page.")
 urlpatterns = [
-    path('', default_urlconf),
+    path('', include('main_app.urls')),
     path('admin/', admin.site.urls),
-    path('welcome/', welcome),
-    path('blog/', Blog),
+
 ]
