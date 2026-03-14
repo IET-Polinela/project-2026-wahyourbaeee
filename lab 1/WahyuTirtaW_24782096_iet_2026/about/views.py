7ddee0fb1e9ce06136540ba18ca7b45d48d9c7e9
Wahyu Tirta Wardana <wwahyutirta@gmail.com>
2026-03-14 11:18:28 +0700

    penambahan templates dalam app dan install apps

--- lab 1/WahyuTirtaW_24782096_iet_2026/about/views.py
+++ lab 1/WahyuTirtaW_24782096_iet_2026/about/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def indexabout(request):
-    return render(request, 'about.html')
+    return render(request, 'about/index.html')
--- lab 1/WahyuTirtaW_24782096_iet_2026/contacts/views.py
+++ lab 1/WahyuTirtaW_24782096_iet_2026/contacts/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def indexcontact(request):
-    return render(request, 'contact.html')
+    return render(request, '/contacts/index.html')
