e824c3c2c8d7c26dc3c7332acec7323e819bc1fc
Wahyu Tirta Wardana <wwahyutirta@gmail.com>
2026-03-14 09:30:51 +0700

    pakai folder template

--- lab 1/WahyuTirtaW_24782096_iet_2026/WahyuTirtaW_24782096_iet_2026/settings.py
+++ lab 1/WahyuTirtaW_24782096_iet_2026/WahyuTirtaW_24782096_iet_2026/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'WahyuTirtaW_24782096_iet_2026.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
