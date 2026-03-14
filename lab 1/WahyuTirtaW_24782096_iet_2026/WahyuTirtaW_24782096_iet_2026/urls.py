336719b6e65d4a650bf86b4eca854816bc7a6b4f
Wahyu Tirta Wardana <wwahyutirta@gmail.com>
2026-03-14 10:07:35 +0700

    buat tampilan html dan perbaiki urls dan view

--- lab 1/WahyuTirtaW_24782096_iet_2026/WahyuTirtaW_24782096_iet_2026/urls.py
+++ lab 1/WahyuTirtaW_24782096_iet_2026/WahyuTirtaW_24782096_iet_2026/urls.py
@@ -2,11 +2,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.debug import default_urlconf
-from django.http import HttpResponse
+
 
 
 urlpatterns = [
-    path('', include('main_app.urls')),
     path('admin/', admin.site.urls),
-
+    path('', include('main_app.urls')),
+    path('contacts/', include('contacts.urls')),
+    path('about/', include('about.urls')),
 ]
--- lab 1/WahyuTirtaW_24782096_iet_2026/contacts/views.py
+++ lab 1/WahyuTirtaW_24782096_iet_2026/contacts/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def indexcontact(request):
+    return render(request, 'contact.html')
