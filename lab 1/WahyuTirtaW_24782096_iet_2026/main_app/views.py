b5cf1710670b42ad5f60fe83fc3e678cc6c71981
Wahyu Tirta Wardana <wwahyutirta@gmail.com>
2026-04-01 17:05:44 +0700

    merubah django tamplate

--- lab 1/WahyuTirtaW_24782096_iet_2026/main_app/views.py
+++ lab 1/WahyuTirtaW_24782096_iet_2026/main_app/views.py
@@ -6,8 +6,8 @@ from .form import ReportForm
 def home(request):
     context = {
         'title': 'Home |T-City',
-        'heading': '',
-        'subheading': 'Halo, ini adalah halaman utama T-MAP (Tirtacity Management & Action Platform) ! T-MAP adalah sebuah ekosistem digital terintegrasi yang dirancang sebagai solusi cerdas dalam pengelolaan infrastruktur dan layanan publik di lingkungan perkotaan. Platform ini menjembatani komunikasi antara warga dan pengelola kota melalui sistem pelaporan isu yang transparan dan terstruktur, mulai dari kendala utilitas hingga kerusakan fasilitas umum. Dengan mengedepankan efisiensi workflow dan validitas data, T-MAP mentransformasi setiap laporan masyarakat menjadi aksi nyata, guna mewujudkan tata kelola kota yang lebih responsif, modern, dan berkelanjutan untuk semua warga', 
+        'heading': 'Selamat Datang di T-Map',
+        'description': 'Halo, ini adalah halaman utama T-MAP (Tirtacity Management & Action Platform) ! T-MAP adalah sebuah ekosistem digital terintegrasi yang dirancang sebagai solusi cerdas dalam pengelolaan infrastruktur dan layanan publik di lingkungan perkotaan. Platform ini menjembatani komunikasi antara warga dan pengelola kota melalui sistem pelaporan isu yang transparan dan terstruktur, mulai dari kendala utilitas hingga kerusakan fasilitas umum. Dengan mengedepankan efisiensi workflow dan validitas data, T-MAP mentransformasi setiap laporan masyarakat menjadi aksi nyata, guna mewujudkan tata kelola kota yang lebih responsif, modern, dan berkelanjutan untuk semua warga', 
         }
     return render(request, 'main_app/index.html', context)
 
