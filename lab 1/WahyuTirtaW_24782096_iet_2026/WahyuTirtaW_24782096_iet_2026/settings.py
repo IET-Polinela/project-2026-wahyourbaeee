a9b643aabce2fbf83bdea72c463c843dd07b8618
Wahyu Tirta Wardana <wwahyutirta@gmail.com>
2026-04-01 14:23:53 +0700

    menambahkan database

--- lab 1/WahyuTirtaW_24782096_iet_2026/WahyuTirtaW_24782096_iet_2026/settings.py
+++ lab 1/WahyuTirtaW_24782096_iet_2026/WahyuTirtaW_24782096_iet_2026/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'WahyuTirtaW_24782096_iet_2026.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'smartcity_db',
+        'USER': 'postgres',
+        'PASSWORD': 'password123',
+        'HOST': 'localhost', 
+        'PORT': '5432',
     }
 }
 
