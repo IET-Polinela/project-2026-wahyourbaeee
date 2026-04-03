f6b5bfe6811fde8e222a4e8a5949a677216e485d
Wahyu Tirta Wardana <wwahyutirta@gmail.com>
2026-04-03 13:25:03 +0700

    eksplor

--- lab 1/WahyuTirtaW_24782096_iet_2026/main_app/urls.py
+++ lab 1/WahyuTirtaW_24782096_iet_2026/main_app/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('add/', views.add_Report, name='report'),
+    path('add/', views.add_Report, name='add_report'),
+    path('list/', views.list_Report, name='list_reports'),
+    path('update/<int:pk>/', views.update_report, name='update_report'),
+    path('delete/<int:pk>/', views.delete_report, name='delete_report'),
 ]
--- lab 1/WahyuTirtaW_24782096_iet_2026/main_app/views.py
+++ lab 1/WahyuTirtaW_24782096_iet_2026/main_app/views.py
@@ -1,6 +1,9 @@
+import django
 from django.shortcuts import redirect, render
 from .models import Report
 from .form import ReportForm
+from django.contrib import messages 
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def home(request):
@@ -16,8 +19,46 @@ def add_Report(request):
         form = ReportForm(request.POST)
         if form.is_valid():
             form.save() # Menyimpan data ke database
+            # Kasih pesan sukses
+            messages.success(request, 'Wih mantap, laporan lo udah masuk ke database!')
             return redirect('home') # Mengarahkan kembali ke halaman utama setelah berhasil menyimpan
     else:
         form = ReportForm()
     return render(request,
                   'main_app/add_report.html',{'form': form})
+
+
+def list_Report(request):
+    # INI BARIS YANG KURANG: Ambil semua data dari PostgreSQL
+    data_laporan = Report.objects.all() 
+    
+    # Masukin data_laporan ke dalam context (dictionary) biar bisa dibaca HTML
+    return render(request, 'main_app/list_report.html', {'laporan': data_laporan})
+
+# Fungsi Update
+def update_report(request, pk):
+    # Ambil data lama berdasarkan ID (pk)
+    report = get_object_or_404(Report, id=pk)
+    
+    if request.method == 'POST':
+        # Isi form pake data POST + instance data lama
+        form = ReportForm(request.POST, instance=report)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Laporan berhasil diperbarui!')
+            return redirect('list_report') # Sesuaikan nama name di urls.py lo
+    else:
+        # Tampilkan form yang udah ada isinya (data lama)
+        form = ReportForm(instance=report)
+    
+    return render(request, 'main_app/add_report.html', {'form': form, 'title': 'Edit Laporan'})
+
+# Fungsi Delete
+def delete_report(request, pk):
+    report = get_object_or_404(Report, id=pk)
+    if request.method == 'POST':
+        report.delete()
+        messages.warning(request, 'Laporan telah dihapus!')
+        return redirect('list_report')
+    
+    return render(request, 'main_app/delete_confirm.html', {'report': report})
