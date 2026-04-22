2532c0706584342d005bb199640de727cdcde2fe
Wahyu Tirta Wardana <wwahyutirta@gmail.com>
2026-04-22 13:43:59 +0700

    Inisialisasi Custom User Model

--- lab 1/WahyuTirtaW_24782096_iet_2026/usermanagement_24782096/models.py
+++ lab 1/WahyuTirtaW_24782096_iet_2026/usermanagement_24782096/models.py
@@ -3,9 +3,7 @@ from django.db import models
 
 # Create your models here.
 class User(AbstractUser):
-    # is_admin default False [cite: 19]
     is_admin = models.BooleanField(default=False)
-    # is_member default True [cite: 20]
     is_member = models.BooleanField(default=True)
 
     def __str__(self):
