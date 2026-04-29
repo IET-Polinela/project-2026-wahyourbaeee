c5b2a84e75d95aeb79e9d74e2ab2bb43a982d997
Wahyu Tirta Wardana <wwahyutirta@gmail.com>
2026-04-29 09:19:02 +0700

    pembuatan html pada dashboard

--- lab 1/WahyuTirtaW_24782096_iet_2026/dashboard_24782096/views.py
+++ lab 1/WahyuTirtaW_24782096_iet_2026/dashboard_24782096/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from main_app.models import Report
+from django.db.models import Count
+from django.views.generic import TemplateView
 
-# Create your views here.
+# View untuk nampilin halaman HTML-nya
+class DashboardView(TemplateView):
+    template_name = 'dashboard/index.html'
+
+# View khusus penyedia data JSON untuk Chart.js
+def dashboard_data(request):
+    # Itung distribusi status
+    status_counts = list(Report.objects.values('status').annotate(total=Count('status')))
+    # Itung distribusi kategori
+    category_counts = list(Report.objects.values('category').annotate(total=Count('category')))
+    
+    return JsonResponse({
+        'status_data': status_counts,
+        'category_data': category_counts
+    })
