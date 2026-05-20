d483ae4e83df73867408f30da1233009554bd83f
Wahyu Tirta Wardana <wwahyutirta@gmail.com>
2026-05-20 14:38:57 +0700

    4. Integrasi ViewSet dan Anonimitas Pelapor

--- lab 1/WahyuTirtaW_24782096_iet_2026/main_app/api_views.py
+++ lab 1/WahyuTirtaW_24782096_iet_2026/main_app/api_views.py
@@ -1,10 +1,16 @@
 from rest_framework import viewsets, permissions
-from rest_framework.exceptions import PermissionDenied
 from .models import Report
 from .serializers import ReportSerializer
-from .permissions import IsOwnerAndDraftOnly
+from .permissions import *
 
 class ReportViewSet(viewsets.ModelViewSet):
-    permission_classes = [permissions.AllowAny]
     queryset = Report.objects.all()
     serializer_class = ReportSerializer
+
+    def get_permissions(self):
+        if self.action in ['update', 'partial_update', 'destroy']:
+            return [permissions.IsAuthenticated(), IsOwnerAndDraftOrReadOnly()]
+        return [permissions.IsAuthenticated()]
+    
+    def perform_create(self, serializer):
+        serializer.save(reporter=self.request.user)
--- lab 1/WahyuTirtaW_24782096_iet_2026/main_app/permissions.py
+++ lab 1/WahyuTirtaW_24782096_iet_2026/main_app/permissions.py
@@ -1,10 +1,10 @@
 from rest_framework import permissions
 
-class IsOwnerAndDraftOnly(permissions.BasePermission):
+class IsOwnerAndDraftOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         # Hanya pemilik laporan yang dapat mengedit atau menghapus jika statusnya 'draft'
         if request.method in permissions.SAFE_METHODS:
             return True
         
-        return obj.owner == request.user and obj.status == 'draft'
+        return obj.reporter == request.user and obj.status == 'DRAFT'
    
