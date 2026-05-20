49fadccef3e5bb4b28239afe6eb6eb28e0601399
Wahyu Tirta Wardana <wwahyutirta@gmail.com>
2026-05-20 14:05:48 +0700

    Pembuatan Custom Permissions (Akses ‘DRAFT’)

--- lab 1/WahyuTirtaW_24782096_iet_2026/main_app/permissions.py
+++ lab 1/WahyuTirtaW_24782096_iet_2026/main_app/permissions.py
@@ -1,20 +1,10 @@
 from rest_framework import permissions
 
 class IsOwnerAndDraftOnly(permissions.BasePermission):
-    """
-    Custom permission untuk memastikan hanya pemilik laporan yang bisa edit/delete,
-    dan itu pun HANYA JIKA status laporan masih 'DRAFT'.
-    """
-
-    def has_permission(self, request, view):
-        # Memastikan user sudah terautentikasi (login)
-        return request.user and request.user.is_authenticated
-
     def has_object_permission(self, request, view, obj):
-        # Pengecekan untuk aksi modifikasi (PUT, PATCH, DELETE)
-        if request.method in ['PUT', 'PATCH', 'DELETE']:
-            # Wajib pemilik laporan DAN statusnya harus 'DRAFT'
-            return obj.reporter == request.user and obj.status == 'DRAFT'
+        # Hanya pemilik laporan yang dapat mengedit atau menghapus jika statusnya 'draft'
+        if request.method in permissions.SAFE_METHODS:
+            return True
         
-        # Untuk GET (Detail), izinkan kalau user sudah login (lolos dari has_permission)
-        return True
+        return obj.owner == request.user and obj.status == 'draft'
+   
