e9c7abb0ff6df9559a4fbf2b8ada4e74b80bc9f0
Wahyu Tirta Wardana <wwahyutirta@gmail.com>
2026-06-17 13:35:52 +0700

    lan14. Konfigurasi Default Schema Class DRF

--- lab1/server_smartcity/smartcity_app/settings.py
+++ lab1/server_smartcity/smartcity_app/settings.py
@@ -147,6 +147,8 @@ MESSAGE_TAGS = {
 }
 
 REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
